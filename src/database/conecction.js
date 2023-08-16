0f14ff8e7cbd97deb0128605076c9039c69d4740
litisrael <isroellit@gmail.com>
2023-08-16 17:19:04 +0300

    local

--- src/database/conecction.js
+++ src/database/conecction.js
@@ -2,57 +2,21 @@ import dotenv from 'dotenv';
 dotenv.config();
 import { Sequelize } from "sequelize";
 
-export const getConnection = async () => {
-  const sequelize = new Sequelize({
-    dialect: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  // database: process.env.POSTGRES_DATABASE,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: 'transport',
-  quoteIdentifiers: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false // Puedes ajustar esto según tus necesidades de seguridad
-    }
-  }
-  });
-
-  return await sequelize.authenticate()
-.then(() => {
-  console.log('Connection has been established successfully.');
-  return sequelize;
-})
-.catch(err => {
-  console.error('Unable to connect to the database:', err);
-  throw err;
-});
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // export const getConnection = async () => {
 //   const sequelize = new Sequelize({
-//     dialect: process.env.db_dialect ,
-//     host: process.env.db_localhost,
-//     port: process.env.db_port,
-//     database: "transoprt",
-//     username: process.env.db_user,
-//     password: process.env.db_password_local,
-//     quoteIdentifiers: false
+//     dialect: 'postgres',
+//   host: process.env.POSTGRES_HOST,
+//   // database: process.env.POSTGRES_DATABASE,
+//   username: process.env.POSTGRES_USER,
+//   password: process.env.POSTGRES_PASSWORD,
+//   database: 'transport',
+//   quoteIdentifiers: false,
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false // Puedes ajustar esto según tus necesidades de seguridad
+//     }
+//   }
 //   });
 
 //   return await sequelize.authenticate()
@@ -72,30 +36,35 @@ export const getConnection = async () => {
 
 
 
-// import path from 'path';
-// export const getConnection = async () => {
-  
-// const AWS_CREDENTIALS_PATH = 'C:/Users/isroe/.aws';
-// const credentials = fs.readFileSync(path.resolve(AWS_CREDENTIALS_PATH, 'credentials'));
 
-//   const [, , , , , password] = credentials.toString().split('\n');
 
-//   const sequelize = new Sequelize({
-//     dialect: 'postgres',
-//     host: 'localhost',
-//     port: 5432,
-//     database: 'transportation',
-//     username: 'postgres',
-//     password: password.trim(),
-//   });
 
-//   try {
-//     await sequelize.authenticate();
-//     console.log('Connection has been established successfully.');
-//     return sequelize;
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//     throw error;
-//   }
-// };
+
+
+
+
+
+export const getConnection = async () => {
+  const sequelize = new Sequelize({
+    dialect: process.env.db_dialect ,
+    host: process.env.db_localhost,
+    port: process.env.db_port,
+    database: "transoprt",
+    username: process.env.db_user,
+    password: process.env.db_password_local,
+    quoteIdentifiers: false
+  });
+
+  return await sequelize.authenticate()
+.then(() => {
+  console.log('Connection has been established successfully.');
+  return sequelize;
+})
+.catch(err => {
+  console.error('Unable to connect to the database:', err);
+  throw err;
+});
+};
+
+
 
