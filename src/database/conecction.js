efe9d3ea74bf103e2c395941af6f60abc3423132
litisrael <isroellit@gmail.com>
2023-09-10 23:26:27 +0300

    este es el push cobn connection vercel

--- src/database/conecction.js
+++ src/database/conecction.js
@@ -8,57 +8,21 @@ import { Sequelize } from "sequelize";
 
 
 
-// export const getConnection = async () => {
-//   const sequelize = new Sequelize({
-//     dialect: 'postgres',
-//   host: process.env.POSTGRES_HOST,
-//   // database: process.env.POSTGRES_DATABASE,
-//   username: process.env.POSTGRES_USER,
-//   password: process.env.POSTGRES_PASSWORD,
-//   database: 'transport',
-//   quoteIdentifiers: false,
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false // Puedes ajustar esto según tus necesidades de seguridad
-//     }
-//   }
-//   });
-
-//   return await sequelize.authenticate()
-// .then(() => {
-//   console.log('Connection has been established successfully.');
-//   return sequelize;
-// })
-// .catch(err => {
-//   console.error('Unable to connect to the database:', err);
-//   throw err;
-// });
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-// local
-
 export const getConnection = async () => {
   const sequelize = new Sequelize({
-    dialect: process.env.db_dialect ,
-    host: process.env.db_localhost,
-    port: process.env.db_port,
-    database: "transoprt",
-    username: process.env.db_user,
-    password: process.env.db_password_local,
-    quoteIdentifiers: false
+    dialect: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  // database: process.env.POSTGRES_DATABASE,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: 'transport',
+  quoteIdentifiers: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Puedes ajustar esto según tus necesidades de seguridad
+    }
+  }
   });
 
   return await sequelize.authenticate()
@@ -71,6 +35,42 @@ export const getConnection = async () => {
   throw err;
 });
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // local
+
+// export const getConnection = async () => {
+//   const sequelize = new Sequelize({
+//     dialect: process.env.db_dialect ,
+//     host: process.env.db_localhost,
+//     port: process.env.db_port,
+//     database: "transoprt",
+//     username: process.env.db_user,
+//     password: process.env.db_password_local,
+//     quoteIdentifiers: false
+//   });
+
+//   return await sequelize.authenticate()
+// .then(() => {
+//   console.log('Connection has been established successfully.');
+//   return sequelize;
+// })
+// .catch(err => {
+//   console.error('Unable to connect to the database:', err);
+//   throw err;
+// });
+// };
 
 
 
